423369dfe9c44128ac041bbec3af9ff335f0ba5f
payand <shooshtariali05@gmail.com>
2024-01-31 18:17:49 +0330

    fix: module import issue resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { OrderController } from './order/order.controller';
-import { OrderService } from './order/order.service';
+
 import { RequestIdMiddleware } from './middleware/set-request-id.middleware';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: 'kafka_client_id',
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'kafka_group_id',
-          },
-        },
-      },
-    ]),
-  ],
-  controllers: [OrderController],
-  providers: [OrderService],
+  imports: [OrderModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
-
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'kafka_client_id',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'kafka_group_id',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
